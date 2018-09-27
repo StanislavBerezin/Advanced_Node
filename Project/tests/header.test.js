1954b3631ed447ca60e38e90d54458f22685a1c4
StanislavBerezin <stanislav_berezin@hotmail.com>
2018-09-27 15:49:55 +1000

    test for auth

---
+++ Project/tests/header.test.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer')
+
+let browser, page
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    })
+    page = await browser.newPage()
+
+    await page.goto('localhost:3000')
+
+})
+
+afterEach(async () => {
+    await browser.close()
+})
+// headless:false will boot the browser
+test("The header has the right text", async () => {
+
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML)
+
+    expect(text).toEqual('Blogster')
+})
+
+test("Clicking login starts authentication process", async () => {
+    await page.click('.right a')
+    const url = await page.url()
+    expect(url).toMatch(/accounts\.google\.com/);
+
+})
